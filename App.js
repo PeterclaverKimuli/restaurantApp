0e7bb50dc80cb6a161f1ce524b783f6716fac1d8
Peterclaver Kimuli <peterclaver.kimuli@gmail.com>
2018-06-08 14:47:00 +0300

    add all the required files

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from "./application/components/login/Login";
+
+export class App extends React.Component{
+
+    render(){
+        return(
+            <Login/>
+        )
+    }
+}
---
+++ application/components/login/Login.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {SectionList, Text, View, StyleSheet} from "react-native"
+
+export default class Login extends React.Component{
+
+}
+
